31c5fb5c3a0e58a291187b06713a27ee906656ba
Codingway-Amber <amberreine7@gmail.com>
2024-08-28 14:34:05 -0500

    pac-man add

--- sketch.js
+++ sketch.js
@@ -1,5 +1,17 @@
 function setup() {
-    createCanvas (100,100);
-    background(150);
-    ellipse(width/2, height)
+    createCanvas (windowWidth,windowHeight);
+    background(0, 0, 90);
+    fill(255,255,0);
+    noStroke();
+    ellipse(250,250,400,400);
+
+    fill(0,0,90)
+    triangle(250,250, 500,100, 500,400);
+
+    fill(0,0,90);
+    ellipse(290, 150, 50, 50);
+
+    fill(255,255,255);
+    ellipse(400, 250, 80, 80);
+    
 }
